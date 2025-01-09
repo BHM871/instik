f2cd1c5e06c7253774551448a5ed86891e8f5f23
Adrian Almeida <adrianalmeida417@gmail.com>
2025-01-09 02:16:15 -0300

    feature: implementing ClassLoader

--- configs/init.php
+++ configs/init.php
@@ -2,63 +2,9 @@
 require_once("./configs/constants.php");
 require_once("./configs/enums.php");
 
+require_once("./system/ClassLoader.php");
 require_once("./system/ViewLoader.php");
 require_once("./system/RouterConfig.php");
 
-function loadClasses($path) {
-	$iterator = new DirectoryIterator($path);
-
-	foreach ($iterator as $info) {
-		if ($info->getFilename() == '.' || $info->getFilename() == '..') {
-			continue;
-		}
-
-		if ($info->isDir()) {
-			loadClasses($info->getPathname());
-			continue;
-		}
-
-		$GLOBALS['classesToLoad'][] = $info->getPathname();
-	}
-}
-
-function addRoutes() {
-	foreach (get_declared_classes() as $className) {
-		$class = new ReflectionClass($className);
-		$attributes = $class->getAttributes(Routable::class);
-		
-		if (empty($attributes)) {
-			continue;
-		}
-
-		$object = $class->newInstance();
-
-		$classUri = $attributes[0]->getArguments()[0];
-		$classUri = preg_replace("/^[^\/]/", '/', $classUri);
-		$classUri = preg_replace("/\/$/", '', $classUri);
-
-		foreach ($class->getMethods() as $method) {
-			$attributes = $method->getAttributes(Route::class);
-
-			if(empty($attributes) || $method->isConstructor() || $method->isDestructor())
-				continue;
-
-			$arguments = $attributes[0]->getArguments();
-
-			$methodUri = $arguments[0];
-			$methodUri = preg_replace("/^[^\/]/", '/', $methodUri);
-			$methodUri = preg_replace("/\/$]/", '', $methodUri);
-
-			$realPath = $classUri.$methodUri;
-			RouterConfig::add($realPath, $arguments[1], $object, $method);
-		}
-	}
-}
-
-loadClasses("./application");
-
-foreach($classesToLoad as $classPath) {
-	include($classPath);
-}
-
-addRoutes();
+ClassLoader::load('./application');
+RouterConfig::configure();
---
+++ system/ClassLoader.php
@@ -0,0 +1,26 @@
+<?php
+
+class ClassLoader {
+
+	public static function load($path) : array {
+		$classes = [];
+		$iterator = new DirectoryIterator($path);
+
+		foreach ($iterator as $info) {
+			if ($info->getFilename() == '.' || $info->getFilename() == '..') {
+				continue;
+			}
+
+			if ($info->isDir()) {
+				ClassLoader::load($info->getPathname());
+				continue;
+			}
+
+			$classes[] = $info->getPathname();
+			include($info->getPathname());
+		}
+
+		return $classes;
+	}
+
+}
