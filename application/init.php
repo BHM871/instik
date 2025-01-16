e70c41ba514bf2f661709f91aebcc6ac1900402a
Adrian Almeida <adrianalmeida417@gmail.com>
2025-01-16 00:04:01 -0300

    feature: implementing database setup

--- application/init.php
+++ application/init.php
@@ -1,3 +1,14 @@
 <?php
 
 // Setup your application here
+
+$db = Database::instance();
+$db->getDefaultConnection()->query(
+	"CREATE DATABASE IF NOT EXISTS instik CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+);
+
+$success = $db->queries(preg_split("/\;\s*/", file_get_contents("./application/configs/setup-database.sql")));
+
+if ($success == false) {
+	$db->query("ROLLBACK");
+}
