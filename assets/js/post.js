c52df9c0b787a13049b0d5eac874c44c190e488c
Adrian Almeida <adrianalmeida417@gmail.com>
2025-02-04 20:42:13 -0300

    chore: implementing JS to send comment

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,11 +1,12 @@
 const ctx = document.getElementById("ctx");
 
 const likeBtns = document.querySelectorAll("button[type='like']");
+const commentBtns = document.querySelectorAll("button[type='send-comment']");
+
+const headers = new Headers();
+headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
 likeBtns.forEach((btn) => {
-	const headers = new Headers();
-	headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-
 	btn.addEventListener("click", (ev) => {
 		const postId = btn.getAttribute("id");
 
@@ -93,3 +94,63 @@ likeBtns.forEach((btn) => {
 		}
 	});
 });
+
+commentBtns.forEach((btn) => {
+	btn.addEventListener("click", (ev) => {
+		const input = btn.previousElementSibling;
+
+		fetch(ctx.value + "/post/comment", {
+			method: "POST",
+			headers: headers,
+			body: new URLSearchParams(Object.entries({
+				postId: input.getAttribute("id"),
+				comment: input.value
+			})).toString()
+		})
+		.then(async (res) => {
+			const type = res.headers.get("Content-Type");
+
+			if (!res.ok || type == null) {
+				notify("Houve algum problema ao adicionar comentário");
+				return;
+			}
+
+			if (!type.includes("json")) {
+				throw new Error(await res.text());
+			}
+
+			const copy = res.clone();
+
+			try {
+				await copy.json();
+				return res.json();
+			} catch (error) {
+				throw new Error(await res.text());
+			}
+		})
+		.then(json => {
+			if (!json.success) {
+				notify("Houve algum erro ao salvar o comentário");
+				return;
+			}
+
+
+			const comment = btn.parentElement.nextElementSibling;
+			comment.innerHTML = comment.innerHTML +
+			'<div class="comment">' +
+				'<div class="comment-user">' + 
+					'<img class="profile" src="' + ctx.value + '/' + json.user.image_path + '" />' +
+					'<small><a>' + json.user.username + '</a></small>' +
+				'</div>' +
+				'<div class="comment-content">' +
+					'<small>' + json.content + '</small>' +
+				'</div>' +
+				'<div class="comment-replies">' +
+				'</div>' +
+			'</div>';
+		})
+		.catch(error => {
+			notify(error.message);
+		})
+	});
+})
